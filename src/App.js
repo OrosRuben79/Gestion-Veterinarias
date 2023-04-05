08052f3b0aa3a08877f25d8907c02a029fda4d72
OrosRuben79 <109885291+OrosRuben79@users.noreply.github.com>
2023-04-04 21:42:59 -0300

    agrego carpeta component en la raiz

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p></p>
+        <a
+          className="App-link"
+          href="https://www.google.com/search?q=image+the+dog&sxsrf=APwXEdf0b2zOYTz85-crlLO4-seeB0NSaw:1680551341511&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi08fLNvY7-AhWTpZUCHceyAnwQ_AUoAXoECAEQAw&biw=1517&bih=730&dpr=0.9."
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+         Aqui pueden ir seleccionando la imagen de mi LandingPage
+        </a>
+        <h1>BIENVENIDOS A LA APP DE VETERINARIAS</h1>
+          <p>Aquí comienza la Hazaña</p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
